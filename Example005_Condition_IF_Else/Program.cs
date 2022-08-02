6b305825e317981bf3db7afed10b3e357770a870
Alex <A1ekcbend@gmail.com>
2022-08-03 00:30:24 +0300

    add Example 005 This is MISHAA

---
+++ Example005_Condition_IF_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "misha")//ToLower -приводит буквы в нижний регистр
+{
+    Console.WriteLine("Yes, this is MISHAA");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
